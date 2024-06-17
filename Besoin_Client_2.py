1329424b956dc77f9a466ab9f24b4c4e78335d75
Asthehis <noemiefournel751@gmail.com>
2024-06-17 16:38:46 +0200

    modification Besoin_Client_2.py

--- Besoin_Client_2.py
+++ Besoin_Client_2.py
@@ -1,3 +1,23 @@
 import sklearn
 import csv
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
+import numpy
+
+arbre = pd.read_csv("Data_Arbre.csv")
+#print(arbre)
+
+# Méthode regression Random Forest, CART...Gini index
+
+x = arbre[['haut_tronc', 'fk_prec_estim', 'tronc_diam', 'haut_tot', 'fk_stadedev']].copy()
+y = arbre[['age_estim']].copy
+print(x)
+print(y)
+
+encoder = OrdinalEncoder()
+x['fk_stadedev'] = encoder.fit_transform(x[['fk_stadedev']])
+print(x['fk_stadedev'].head())
+
+x_norm = (x - x.min()) / (x.max() - x.min())
+print(x_norm)
 
