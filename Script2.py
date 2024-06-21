bd1c7385d694f76f0a8e5a8ad49146e291bdc635
Asthehis <noemiefournel751@gmail.com>
2024-06-21 03:42:26 +0200

    modif script2

--- Script2.py
+++ Script2.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import json
-import pickle 
+import pickle
+import csv
 
 # Récupération des données
 arbre = pd.read_csv("Data_Arbre.csv")
@@ -13,7 +14,7 @@ def open_pickle(fileName):
 def prediction2(dataFrame):
 
     # Importation des données
-    modeles = open_pickle('dict_pickle')
+    modeles = open_pickle('dict_pickle2')
 
     # Lecture des données
     data = pd.read_json(dataFrame)
@@ -36,32 +37,57 @@ def prediction2(dataFrame):
     x = data[['haut_tronc', 'fk_stadedev', 'tronc_diam', 'haut_tot', 'fk_nomtech']].copy()
     
     # Vérifier que les données soient normalisées, normaliser sinon
-    SC = modeles['scaler']
-    x_norm = SC.transform(x)
+    SC_x = modeles['scaler_x']
+    x_norm = SC_x.transform(x)
 
     # Prédictions
     RF = modeles['RandomForest']
     CART = modeles['DecisionTreeRegressor']
     GBR = modeles['GradientBoostingRegression']
+    ETR = modeles['ExtraTreesRegressor']
     RF_pred = RF.predict(x_norm)
     CART_pred = CART.predict(x_norm)
     GBR_pred = GBR.predict(x_norm)
+    ETR_pred = ETR.predict(x_norm)
 
 
     # Les valeurs prédites seront normalisées, les "dénormaliser"
-    RF_pred = SC.inverse_transform(RF_pred.reshape(-1, 1)).ravel()
-    CART_pred = SC.inverse_transform(CART_pred.reshape(-1, 1)).ravel()
-    GBR_pred =SC.inverse_transform(GBR_pred.reshape(-1, 1)).ravel()
+    SC_y = modeles['scaler_y']
+    RF_pred = SC_y.inverse_transform(RF_pred.reshape(-1, 1)).ravel()
+    CART_pred = SC_y.inverse_transform(CART_pred.reshape(-1, 1)).ravel()
+    GBR_pred =SC_y.inverse_transform(GBR_pred.reshape(-1, 1)).ravel()
+    ETR_pred =SC_y.inverse_transform(GBR_pred.reshape(-1, 1)).ravel()
 
     # Création d'un dictionnaire pour renvoyer les valeurs
     predictions = {
-        'RandomForest' : RF_pred,
-        'DecisionTreeRegressor' : CART_pred,
-        'GradientBoostingRegression' : GBR_pred
+        'RandomForest' : RF_pred.tolist(),
+        'DecisionTreeRegressor' : CART_pred.tolist(),
+        'GradientBoostingRegression' : GBR_pred.tolist(),
+        'ExtraTreesRegressor' : ETR_pred.tolist()
     }
 
-    json_predictions = json.dump(predictions)
+    json_predictions = json.dumps(predictions)
 
     return json_predictions
 
-prediction2(json.dump(arbre))
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+      
+    #read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+  
+    #convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+
+csv_to_json('Data_Arbre.csv', 'Data_Arbre.json')
+pred = prediction2('Data_Arbre.json')
+print(pred)
